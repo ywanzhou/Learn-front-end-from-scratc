7d98f37b5312ece2eed9b1e486b252f97f10a03a
Zhou-Zhonghua <seecode@qq.com>
2021-08-08 18:43:08 +0800

    🌸switch语句

---
+++ 01_JavaScript基础语法/06_条件语句/src/08_default.js
@@ -0,0 +1,31 @@
+var date = 1
+
+switch (date) {
+    case 1:
+        console.log("今天吃烤鸭");
+        break;
+    case 2:
+        console.log("今天吃酱牛肉");
+        break;
+    default:
+        console.log("您的输入有误");
+        break;
+    case 3:
+        console.log("今天吃辣子鸡");
+        break;
+    case 4:
+        console.log("今天吃红烧肉");
+        break;
+    case 5:
+        console.log("今天吃土豆鸡块");
+        break;
+    case 6:
+        console.log("今天火锅");
+        break;
+    case 7:
+        console.log("今天吃外卖");
+        break;
+}
+
+// 最终结果
+// 今天吃烤鸭

a6fc68442aea50936b690219bc5ccac1dd5b1ff3
Zhou-Zhonghua <seecode@qq.com>
2021-08-08 16:59:17 +0800

    🌸if语句

---
+++ 01_JavaScript基础语法/06_条件语句/src/02_if语句.js
@@ -0,0 +1,21 @@
+// var x = 10
+// var y = 9
+
+// // 打印 x y 中最大的值
+// if (x > y) {
+//     console.log("最大值为x，x的值为：" + x)
+// }
+
+// if (x < y) {
+//     console.log("最大值为y，y的值为：" + y)
+// }
+// // 执行结果如下：
+// // 最大值为x，x的值为：10
+
+var x = 10
+var y = 9
+if (x > y)
+    console.log("最大值为x，x的值为：" + x)
+
+// 也可以编写在一行
+if (x < y) console.log("最大值为y，y的值为：" + y)

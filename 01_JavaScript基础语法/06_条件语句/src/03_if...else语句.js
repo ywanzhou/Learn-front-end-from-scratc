f1f93590e5eeb9e7dff083a3f31754fd24201ff3
Zhou-Zhonghua <seecode@qq.com>
2021-08-08 17:24:33 +0800

    if...else语句

---
+++ 01_JavaScript基础语法/06_条件语句/src/03_if...else语句.js
@@ -0,0 +1,13 @@
+var x = 10
+var y = 9
+if (x > y) {
+    console.log("最大值为x，x的值为：" + x)
+} else {
+    console.log("最大值为y，y的值为：" + y)
+}
+
+if (x > y)
+    console.log("最大值为x，x的值为：" + x)
+else
+    console.log("最大值为y，y的值为：" + y)
+

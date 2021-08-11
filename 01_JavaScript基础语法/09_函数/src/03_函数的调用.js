b87ae7b4ea40c8649b5a56c42af0ed631a661a1c
Zhou-Zhonghua <seecode@qq.com>
2021-08-11 21:13:25 +0800

    🌸函数的调用

---
+++ 01_JavaScript基础语法/09_函数/src/03_函数的调用.js
@@ -0,0 +1,15 @@
+function print () {
+    console.log('这是一个函数');
+}
+
+// 调用函数
+print()
+
+// 执行结果
+// 这是一个函数
+
+console.log(print) // [Function: print]
+
+
+input() // ReferenceError: input is not defined
+

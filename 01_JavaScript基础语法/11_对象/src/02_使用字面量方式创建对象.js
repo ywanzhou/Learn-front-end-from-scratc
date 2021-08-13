e21c50bd8f5fa8997877adb30a116d7154dfe37d
Zhou-Zhonghua <seecode@qq.com>
2021-08-13 21:07:29 +0800

    🌸使用字面量方式创建对象

---
+++ 01_JavaScript基础语法/11_对象/src/02_使用字面量方式创建对象.js
@@ -0,0 +1,8 @@
+// obj 表示对象的名字
+var obj = {
+    name: '杯子', // 表示属性
+    drink: function () { // 表示方法
+        console.log('杯子可以用来喝水.')
+    }
+}
+console.log(obj) // { name: '杯子', drink: [Function: drink] }

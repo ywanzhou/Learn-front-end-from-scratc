922e4c43bb9dd30f99941bf596b929afea2b8bd4
Zhou-Zhonghua <seecode@qq.com>
2021-08-14 15:30:31 +0800

    🌸大小写转换

---
+++ 01_JavaScript基础语法/12_String对象/src/01_转换大小写.js
@@ -0,0 +1,8 @@
+var msg = 'Hello World'
+
+var lowerMsg = msg.toLowerCase()
+var upperMsg = msg.toUpperCase()
+
+console.log(msg);// Hello World
+console.log(lowerMsg);// hello world
+console.log(upperMsg);// HELLO WORLD

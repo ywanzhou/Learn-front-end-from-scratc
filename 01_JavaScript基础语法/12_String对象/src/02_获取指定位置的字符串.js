931b5e84a3d68b6487912614d7ee7930e79265d8
Zhou-Zhonghua <seecode@qq.com>
2021-08-14 15:37:28 +0800

    🌸获取指定位置的字符串

---
+++ 01_JavaScript基础语法/12_String对象/src/02_获取指定位置的字符串.js
@@ -0,0 +1,7 @@
+var str = 'Hello World!'
+
+var charAt = str.charAt(2)
+var charCodeAt = str.charCodeAt(2)
+var codePointAt = str.codePointAt(2)
+
+console.log(charAt, charCodeAt, codePointAt)  // l 108 108

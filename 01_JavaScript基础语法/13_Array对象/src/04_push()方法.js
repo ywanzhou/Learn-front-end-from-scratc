a1c50c69bf6eee7c9880a45c8a9369ed8d476677
Zhou-Zhonghua <seecode@qq.com>
2021-08-15 16:53:08 +0800

    🌸队列操作

---
+++ 01_JavaScript基础语法/13_Array对象/src/04_push()方法.js
@@ -0,0 +1,6 @@
+var arr = [100, true, '这是一个字符串']
+
+var count = arr.push(200)
+console.log(arr) // [ 100, true, '这是一个字符串', 200 ]
+console.log('添加新元素之后的数组长度为: ' + count) // 添加新元素之后的数组长度为: 4
+
---
+++ 01_JavaScript基础语法/13_Array对象/src/05_pop()方法.js
@@ -0,0 +1,6 @@
+var arr = [100, true, '这是一个字符串']
+
+// 删除数组最后一个元素
+var result = arr.pop()
+console.log('数组删除的元素为: ' + result) // 数组删除的元素为: 这是一个字符串
+console.log(arr) // [ 100, true ]

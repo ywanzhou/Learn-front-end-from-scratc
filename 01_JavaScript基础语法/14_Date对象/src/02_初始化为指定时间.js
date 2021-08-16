96b7ad50ef0becbacd18dc61f8362550b4b89a64
Zhou-Zhonghua <seecode@qq.com>
2021-08-16 11:36:57 +0800

    创建Date对象

---
+++ 01_JavaScript基础语法/14_Date对象/src/02_初始化为指定时间.js
@@ -0,0 +1,9 @@
+var date1 = new Date(2021, 0)
+var date2 = new Date(2021, 0, 1)
+var date3 = new Date(2021, 0, 1, 0)
+var date4 = new Date(2021, 0, 1, 0, 0, 0, 0)
+
+console.log(date1) // Fri Jan 01 2021 00:00:00 GMT+0800
+console.log(date2) // Fri Jan 01 2021 00:00:00 GMT+0800
+console.log(date3) // Fri Jan 01 2021 00:00:00 GMT+0800
+console.log(date4) // Fri Jan 01 2021 00:00:00 GMT+0800

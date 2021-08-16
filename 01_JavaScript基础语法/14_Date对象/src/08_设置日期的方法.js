e6d4dd8aa27daff6a955d8a03b64ea1bdb66b61b
Zhou-Zhonghua <seecode@qq.com>
2021-08-16 14:06:55 +0800

    设置日期的方法

---
+++ 01_JavaScript基础语法/14_Date对象/src/08_设置日期的方法.js
@@ -0,0 +1,5 @@
+var date = new Date()
+console.log('当前时间: ' + date) // 当前时间: Mon Aug 16 2021 13:56:58 GMT+0800 (中国标准时间)
+
+date.setDate(24)
+console.log('修改后的时间: ' + date) // 修改后的时间: Tue Aug 24 2021 13:56:58 GMT+0800 (中国标准时间)

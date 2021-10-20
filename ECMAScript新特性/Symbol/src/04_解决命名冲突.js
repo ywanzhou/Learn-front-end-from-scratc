06a7f846cec28a0cdd9058bc690da264c50a0f12
Zhou-Zhonghua <seecode@qq.com>
2021-10-20 21:36:50 +0800

    解决命名冲突

--- ECMAScript新特性/Symbol/src/04_解决命名冲突.js
+++ ECMAScript新特性/Symbol/src/04_解决命名冲突.js
@@ -4,11 +4,11 @@ cache['foo'] = 123
 // b.js
 cache['foo'] = 234
 
-console.log(cache) // {foo: 234}
+console.log(cache) // { foo: 234 }
 // ==============================================
 // a.js
 cache['a_foo'] = 123
 // b.js
 cache['b_foo'] = 234
 
-console.log(cache) // {a_foo: 123, b_foo: 234}
+console.log(cache) // { foo: 234, a_foo: 123, b_foo: 234 }

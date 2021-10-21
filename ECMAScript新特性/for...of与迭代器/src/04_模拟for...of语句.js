d569f1c69e8d6753a52e471424a1777ecaabb3b5
Zhou-Zhonghua <seecode@qq.com>
2021-10-21 16:48:37 +0800

    模拟for...of语句

---
+++ ECMAScript新特性/for...of与迭代器/src/04_模拟for...of语句.js
@@ -0,0 +1,20 @@
+function forOf(obj, cb) {
+    let iterable, result
+
+    if (typeof obj[Symbol.iterator] !== 'function')
+        throw new TypeError(result + ' is not iterable')
+    if (typeof cb !== 'function') throw new TypeError('cb must be callable')
+
+    iterable = obj[Symbol.iterator]()
+
+    result = iterable.next()
+    while (!result.done) {
+        cb(result.value)
+        result = iterable.next()
+    }
+}
+
+let arr = [1, 2, 3, 4]
+forOf(arr, item => {
+    console.log(item)
+})

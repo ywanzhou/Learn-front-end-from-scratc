3c5e272eb599241de9065f63c610b820f80369fb
Zhou-Zhonghua <seecode@qq.com>
2021-10-30 11:07:23 +0800

    只读属性

---
+++ TypeScript/05_接口/03_只读属性.ts
@@ -0,0 +1,15 @@
+;(function () {
+  interface Person {
+    // 将 name 设置为只读
+    readonly name: string
+  }
+  // 定义一个 person 对象
+  let person = {
+    name: '彼岸繁华',
+  }
+  // 定义 set 方法
+  function setPersonName(person: Person, name: string): void {
+    person.name = name // Cannot assign to 'name' because it is a read-only property.
+  }
+  setPersonName(person, '彼岸繁華')
+})()

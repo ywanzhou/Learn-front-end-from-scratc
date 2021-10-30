f4c398c6b876af5b9488dd185c1644ac52d7b93d
Zhou-Zhonghua <seecode@qq.com>
2021-10-30 14:30:45 +0800

    重写父类方法

--- TypeScript/06_类/03_重写父类方法.ts
+++ TypeScript/06_类/03_重写父类方法.ts
@@ -1,29 +1,33 @@
-// 定义一个 Person类
-class Person {
-  constructor(public name: string) {}
-  // 定义一个方法
-  sayMy() {
-    console.log(`我的名字: ${this.name}`)
+;(function () {
+  // 定义一个 Person类
+  class Person {
+    constructor(public name: string) {}
+    // 定义一个方法
+    sayMy() {
+      console.log(`我的名字: ${this.name}`)
+    }
   }
-}
-// 定义一个 Adult 类继承于 Person 类
-class Adult extends Person {
-  constructor(public age: number) {
-    super('一碗周')
-  } // 重写父类方法
-  sayMy() {
-    console.log(`我的名字: ${this.name} 年龄: ${this.age}`)
+  // 定义一个 Adult 类继承于 Person 类
+  class Adult extends Person {
+    constructor(public age: number) {
+      super('一碗周')
+    } // 重写父类方法
+    sayMy() {
+      console.log(`我的名字: ${this.name} 年龄: ${this.age}`)
+    }
   }
-}
-// 定义一个 Programmer 类继承于 Adult 类
-class Programmer extends Adult {
-  constructor(public hobby: string) {
-    super(18)
-  } // 重写父类方法
-  sayMy() {
-    console.log(`我的名字: ${this.name} 年龄: ${this.age} 爱好: ${this.hobby}`)
+  // 定义一个 Programmer 类继承于 Adult 类
+  class Programmer extends Adult {
+    constructor(public hobby: string) {
+      super(18)
+    } // 重写父类方法
+    sayMy() {
+      console.log(
+        `我的名字: ${this.name} 年龄: ${this.age} 爱好: ${this.hobby}`,
+      )
+    }
   }
-}
-// 类的实例化
-const programmer = new Programmer('coding')
-programmer.sayMy() // 我的名字: 一碗周 年龄: 18 爱好: coding
+  // 类的实例化
+  const programmer = new Programmer('coding')
+  programmer.sayMy() // 我的名字: 一碗周 年龄: 18 爱好: coding
+})()

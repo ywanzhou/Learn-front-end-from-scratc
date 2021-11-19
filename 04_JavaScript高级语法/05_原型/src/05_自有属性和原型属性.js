f2768ab0c10cb9e9a717681a6272a98165accbe6
Zhou-Zhonghua <seecode@qq.com>
2021-11-19 17:29:16 +0800

    自由属性原型属性

---
+++ 04_JavaScript高级语法/05_原型/src/05_自有属性和原型属性.js
@@ -0,0 +1,20 @@
+//构造函数
+function Fun() {}
+//添加原型属性和方法
+Fun.prototype.name = '一碗周'
+Fun.prototype.print = function () {
+  console.log('this is function')
+}
+
+//通过构造函数创建对象
+var fun = new Fun()
+//为对象添加属性和方法
+fun.name = '一碗周'
+fun.SayMe = function () {
+  console.log('this is SayMe')
+}
+
+//访问属性和方法
+console.log(fun.name) // 一碗周
+fun.SayMe() // this is SayMe
+fun.print() // this is function

2ff00e81c8d84a95ae31a367dc5272018a9f6e9e
Zhou-Zhonghua <seecode@qq.com>
2021-11-22 23:30:02 +0800

    继承组合式继承

---
+++ 04_JavaScript高级语法/07_继承/src/07_继承组合式继承.js
@@ -0,0 +1,37 @@
+function inheritPrototype(ChildClass, ParentClass) {
+  var prototype = Object.create(ParentClass.prototype) // 创建对象，创建父类原型的一个副本
+  prototype.constructor = ChildClass // 增强对象，弥补因重写原型而失去的默认的 constructor 属性
+  ChildClass.prototype = prototype // 指定对象，将新创建的对象赋值给子类的原型
+}
+
+// 父类初始化实例属性和原型属性
+function ParentClass(name) {
+  this.name = name
+  this.colors = ['red', 'blue', 'green']
+}
+ParentClass.prototype.sayName = function () {
+  console.log(this.name)
+}
+
+// 借用构造函数传递增强子类实例属性（支持传参和避免篡改）
+function ChildClass(name, age) {
+  ParentClass.call(this, name)
+  this.age = age
+}
+
+// 将父类原型指向子类
+inheritPrototype(ChildClass, ParentClass)
+
+// 新增子类原型属性
+ChildClass.prototype.sayAge = function () {
+  console.log(this.age)
+}
+
+var instance1 = new ChildClass('一碗周', 19)
+var instance2 = new ChildClass('一碗甜', 18)
+
+instance1.colors.push('black')
+console.log(instance1.colors) // [ 'red', 'blue', 'green', 'black' ]
+instance1.sayName() // 一碗周
+instance2.colors.push('yellow')
+console.log(instance2.colors) // [ 'red', 'blue', 'green', 'yellow' ]

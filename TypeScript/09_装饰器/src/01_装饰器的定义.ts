ee3757e5bf800fc7c6359a0bf0686b2db637e4ab
Zhou-Zhonghua <seecode@qq.com>
2021-11-29 18:28:18 +0800

    装饰器的定义

---
+++ TypeScript/09_装饰器/src/01_装饰器的定义.ts
@@ -0,0 +1,22 @@
+// // 定义一个函数作为装饰器函数使用
+// function testDecorator() {}
+
+// // 通过@符号使用装饰器
+// @testDecorator()
+
+// // 装饰器工厂，返回值是一个函数
+// function testDecorator() {
+//   return function() {}
+// }
+
+// // 通过@符号 + 函数调用的方式使用装饰器
+// @testDecorator()
+
+// // 定义两个装饰器函数
+// function setName() {}
+// function setAge() {}
+
+// // 使用装饰器
+// @setName
+// @setAge
+// class Person {}

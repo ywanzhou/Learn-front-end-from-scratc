8cab86735ff4e28e6f538e61d01d4f8488f7ad40
Zhou-Zhonghua <seecode@qq.com>
2021-11-30 22:10:22 +0800

    gulp demo init

---
+++ 前端工程化/03_自动化构建/demo2/src/assets/scripts/main.js
@@ -0,0 +1,13 @@
+// TODO: site logics
+
+// $(($) => {
+//   const $body = $('html, body')
+
+//   $('#scroll_top').on('click', () => {
+//     $body.animate({ scrollTop: 0 }, 600)
+//     return false
+//   })
+// })
+
+console.log("1111111")
+console.log("1111111")

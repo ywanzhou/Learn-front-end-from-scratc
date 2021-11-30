fd7fa6f8735e2ff5e827d689577dac0de26ed8d0
Zhou-Zhonghua <seecode@qq.com>
2021-11-30 21:55:51 +0800

    文件流

--- 前端工程化/03_自动化构建/demo1/gulpfile.js
+++ 前端工程化/03_自动化构建/demo1/gulpfile.js
@@ -1,16 +1,30 @@
 // Gulp的入口文件
 const fs = require('fs')
-exports.stream = done => {
-  // 读取文件的文件流对象
-  const readStream = fs.createReadStream('package.json')
-  // 写入文件的文件流对象
-  const writeStream = fs.createWriteStream('temp.txt')
-  // 文件复制从读入通过管道倒入到写入里面
-  readStream.pipe(writeStream)
-  // 把readStream返回
-  // return readStream
-  // 上面的return类似于监听了end事件执行done
-  readStream.on('end', () => {
-    done()
+const { Transform } = require('stream')
+
+exports.default = () => {
+  // 文件读取流
+  const read = fs.createReadStream('style.css')
+  // 文件写入流
+  const write = fs.createWriteStream('style.min.css')
+  // 文件转换流
+  const transform = new Transform({
+    transform: (chunk, encoding, callback) => {
+      // ★★★核心转换过程实现★★★
+      // chunk => 读取流中读取到的内容（Buffer）
+      // 使用toString将Buffer数组转化成字符串
+      const input = chunk.toString()
+      // 替换掉空白字符和css注释
+      // 将转换后的结果放在output变量中
+      const output = input.replace(/\s+/g, '').replace(/\/\*.+?\*\//g, '')
+      // 执行callback的时候返回，错误优先第一个传错误参数，没有的话传null
+      // output是成功之后作为结果导出
+      callback(null, output)
+    },
   })
+
+  // 文件复制从读入通过管道先转换，后倒入到写入里面
+  read.pipe(transform).pipe(write)
+
+  return read
 }

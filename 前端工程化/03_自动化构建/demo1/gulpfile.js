552bf39437691893e7362984d08b328feb4e5678
Zhou-Zhonghua <seecode@qq.com>
2021-11-30 21:38:21 +0800

    stream流

--- 前端工程化/03_自动化构建/demo1/gulpfile.js
+++ 前端工程化/03_自动化构建/demo1/gulpfile.js
@@ -1,10 +1,16 @@
 // Gulp的入口文件
-const delay = time => {
-  return new Promise((resolve, reject) => {
-    setTimeout(resolve, time)
+const fs = require('fs')
+exports.stream = done => {
+  // 读取文件的文件流对象
+  const readStream = fs.createReadStream('package.json')
+  // 写入文件的文件流对象
+  const writeStream = fs.createWriteStream('temp.txt')
+  // 文件复制从读入通过管道倒入到写入里面
+  readStream.pipe(writeStream)
+  // 把readStream返回
+  // return readStream
+  // 上面的return类似于监听了end事件执行done
+  readStream.on('end', () => {
+    done()
   })
-}
-exports.default = async () => {
-  await delay(1000)
-  console.log('async task~')
 }

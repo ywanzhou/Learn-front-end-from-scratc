8f1de5f22c496d4d020106ffd145b2dc0b8b771c
Zhou-Zhonghua <seecode@qq.com>
2021-11-30 21:29:21 +0800

    async/await

--- 前端工程化/03_自动化构建/demo1/gulpfile.js
+++ 前端工程化/03_自动化构建/demo1/gulpfile.js
@@ -1,13 +1,10 @@
 // Gulp的入口文件
-exports.default = () => {
-  console.log('promise task~')
-  if (true) {
-    // 这里通过promise的resolve方法返回一个成功的promise
-    // 一旦resolve了，那么任务就结束了
-    // resolve里面不需要传参数，因为gulp会忽略这个值
-    return Promise.resolve()
-  } else {
-    // 通过reject去触发一个异常
-    return Promise.reject(new Error('promise failed~'))
-  }
+const delay = time => {
+  return new Promise((resolve, reject) => {
+    setTimeout(resolve, time)
+  })
+}
+exports.default = async () => {
+  await delay(1000)
+  console.log('async task~')
 }

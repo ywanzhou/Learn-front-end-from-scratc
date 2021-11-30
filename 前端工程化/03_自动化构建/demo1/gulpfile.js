7fce25ae3a6fc53ffef081dba39ea8a792456844
Zhou-Zhonghua <seecode@qq.com>
2021-11-30 21:26:53 +0800

    promise回调

--- 前端工程化/03_自动化构建/demo1/gulpfile.js
+++ 前端工程化/03_自动化构建/demo1/gulpfile.js
@@ -1,5 +1,13 @@
 // Gulp的入口文件
-exports.error = done => {
-  console.log('error task~')
-  done(new Error('task failed~'))
+exports.default = () => {
+  console.log('promise task~')
+  if (true) {
+    // 这里通过promise的resolve方法返回一个成功的promise
+    // 一旦resolve了，那么任务就结束了
+    // resolve里面不需要传参数，因为gulp会忽略这个值
+    return Promise.resolve()
+  } else {
+    // 通过reject去触发一个异常
+    return Promise.reject(new Error('promise failed~'))
+  }
 }

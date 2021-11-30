bab9b7649639dff91a4c34467220fee38da68ea4
Zhou-Zhonghua <seecode@qq.com>
2021-11-30 22:02:17 +0800

    输入输出流API

--- 前端工程化/03_自动化构建/demo1/gulpfile.js
+++ 前端工程化/03_自动化构建/demo1/gulpfile.js
@@ -1,30 +1,15 @@
-// Gulp的入口文件
-const fs = require('fs')
-const { Transform } = require('stream')
+const { src, dest } = require('gulp')
+const cleanCss = require('gulp-clean-css')
+const rename = require('gulp-rename')
 
-exports.default = () => {
-  // 文件读取流
-  const read = fs.createReadStream('style.css')
-  // 文件写入流
-  const write = fs.createWriteStream('style.min.css')
-  // 文件转换流
-  const transform = new Transform({
-    transform: (chunk, encoding, callback) => {
-      // ★★★核心转换过程实现★★★
-      // chunk => 读取流中读取到的内容（Buffer）
-      // 使用toString将Buffer数组转化成字符串
-      const input = chunk.toString()
-      // 替换掉空白字符和css注释
-      // 将转换后的结果放在output变量中
-      const output = input.replace(/\s+/g, '').replace(/\/\*.+?\*\//g, '')
-      // 执行callback的时候返回，错误优先第一个传错误参数，没有的话传null
-      // output是成功之后作为结果导出
-      callback(null, output)
-    },
-  })
-
-  // 文件复制从读入通过管道先转换，后倒入到写入里面
-  read.pipe(transform).pipe(write)
-
-  return read
+exports.default = done => {
+  // 读取流，参数是文件路径，比较强大的地方是这里可以使用通配符匹配多个文件
+  src('*.css')
+    // 压缩代码
+    .pipe(cleanCss())
+    // 修改文件类型
+    .pipe(rename({ extname: '.min.css' }))
+    // 输出流，参数是输出路径
+    .pipe(dest('dist'))
+  done()
 }

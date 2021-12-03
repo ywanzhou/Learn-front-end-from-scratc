ddcc9748c70df112d6537e755c3842b1c74721a9
Zhou-Zhonghua <seecode@qq.com>
2021-12-03 22:51:11 +0800

    输出版本号

--- 前端工程化/02_脚手架/ywz/bin/ywz.js
+++ 前端工程化/02_脚手架/ywz/bin/ywz.js
@@ -1,3 +1,7 @@
 #! /usr/bin/env node
+const { program } = require('commander')
+const { version } = require('../package.json')
 
-console.log(process.argv)
+// .version() 方法用于设置版本号，当在命令行中执行 --version 或者 -V 时，显示的版本
+// .parse() 用于解析命名行参数，默认值为 process.argv * 重要
+program.version(version).parse()

6a7f8bd0bc211008e0cbff2e4217f3a97ef71444
Zhou-Zhonghua <seecode@qq.com>
2021-12-05 15:41:33 +0800

    调整ora版本

--- 前端工程化/02_脚手架/ywz/lib/index.js
+++ 前端工程化/02_脚手架/ywz/lib/index.js
@@ -1,4 +1,5 @@
 const inquirer = require('inquirer')
+const ora = require('ora')
 
 module.exports = async name => {
   let { projectName } = await inquirer.prompt({
@@ -11,17 +12,22 @@ module.exports = async name => {
     // 默认值
     default: name,
   })
-  let { license } = await inquirer.prompt({
-    // 问题的类型，list 表示可以选择
-    type: 'list',
-    // 答案的 key
-    name: 'license',
-    // 问题是什么
-    message: 'Choose a license',
-    // 支持选择的选项
-    choices: ['LGPL', 'Mozilla', 'GPL', 'BSD', 'MIT', 'Apache'],
-    // 默认值
-    default: 'MIT',
-  })
-  console.log(projectName, license)
+  const spinner = ora('开始加载...').start()
+  setTimeout(() => {
+    console.log('\n项目名称是：' + projectName)
+    spinner.succeed('加载完毕')
+  }, 3000)
+  // let { license } = await inquirer.prompt({
+  //   // 问题的类型，list 表示可以选择
+  //   type: 'list',
+  //   // 答案的 key
+  //   name: 'license',
+  //   // 问题是什么
+  //   message: 'Choose a license',
+  //   // 支持选择的选项
+  //   choices: ['LGPL', 'Mozilla', 'GPL', 'BSD', 'MIT', 'Apache'],
+  //   // 默认值
+  //   default: 'MIT',
+  // })
+  // console.log(projectName, license)
 }

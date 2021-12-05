4e49d993be989406dca5059dd9fd31a386b40222
Zhou-Zhonghua <seecode@qq.com>
2021-12-05 22:39:03 +0800

    获取模板仓库

--- 前端工程化/02_脚手架/ywz/lib/index.js
+++ 前端工程化/02_脚手架/ywz/lib/index.js
@@ -1,3 +1,4 @@
+const { default: axios } = require('axios')
 const inquirer = require('inquirer')
 const ora = require('ora')
 
@@ -21,6 +22,30 @@ const loading = callback => {
     }
   }
 }
+/**
+ * @description: 获取仓库列表
+ * @param {string} username 被获取的用户名
+ * @returns {Array} 仓库列表
+ */
+const fetchRepoList = async username => {
+  let { data } = await axios.get(
+    `https://api.github.com/users/${username}/repos`,
+  )
+  return data.map(item => item.name)
+}
+
+/**
+ * @description: 获取 tags 列表
+ * @param {string} username 需要获取的用户名
+ * @param {string} repoName 需要获取的仓库名称
+ * @returns {Array} tags 列表
+ */
+const fetchTagList = async (username, repoName) => {
+  let { data } = await axios.get(
+    `https://api.github.com/repos/${username}/${repoName}/tags`,
+  )
+  return data.map(item => item.name)
+}
 
 module.exports = async name => {
   let { projectName } = await inquirer.prompt({
@@ -33,11 +58,28 @@ module.exports = async name => {
     // 默认值
     default: name,
   })
-  let res = await loading(() => {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        reject(200)
-      }, 3000)
+  // 获取仓库列表
+  let repos = await loading(fetchRepoList)('pacpc')
+  // 选择仓库列表
+  let { repoName } = await inquirer.prompt({
+    type: 'list',
+    name: 'repoName',
+    message: 'Choose a template',
+    choices: repos,
+  })
+  // 获取所有 tags
+  let tags = await loading(fetchTagList)('pacpc', repoName)
+
+  // 如果有多个版本，用户选择多个版本，没有多个版本可以直接下载
+  if (tags.length) {
+    // 存在
+    let { tagVersion } = await inquirer.prompt({
+      type: 'list',
+      name: 'tagVersion',
+      message: 'Choose the target version',
+      choices: tags,
     })
-  })()
+    repoName += `#${tagVersion}`
+  }
+  console.log(projectName, repoName)
 }

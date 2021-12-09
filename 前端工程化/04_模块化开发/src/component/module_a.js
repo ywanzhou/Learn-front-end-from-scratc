b2b1cdbcac8d0afa97712baf38f6434ba71cae89
Zhou-Zhonghua <seecode@qq.com>
2021-12-09 11:42:29 +0800

    IIFE

--- 前端工程化/04_模块化开发/src/component/module_a.js
+++ 前端工程化/04_模块化开发/src/component/module_a.js
@@ -1,6 +1,14 @@
-let moduleA = {
-  name: '一碗周',
-  handle() {
-    console.log(this.name)
-  },
-}
+// let moduleA = {
+//   name: '一碗周',
+//   handle() {
+//     console.log(this.name)
+//   },
+// }
+
+;(function () {
+  let name = '一碗周'
+  function handle() {
+    console.log(name)
+  }
+  window.moduleA = { handle }
+})()
--- 前端工程化/04_模块化开发/src/component/module_b.js
+++ 前端工程化/04_模块化开发/src/component/module_b.js
@@ -1,6 +1,14 @@
-let moduleB = {
-  name: '一碗粥',
-  handle() {
-    console.log(this.name)
-  },
-}
+// let moduleB = {
+//   name: '一碗粥',
+//   handle() {
+//     console.log(this.name)
+//   },
+// }
+
+;(function () {
+  let name = '一碗粥'
+  function handle() {
+    console.log(name)
+  }
+  window.moduleB = { handle }
+})()

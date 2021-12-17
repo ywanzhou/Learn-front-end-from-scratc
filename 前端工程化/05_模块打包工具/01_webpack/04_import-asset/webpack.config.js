0a33362c687ddaa702e6392d4f3107b161b3ca37
Zhou-Zhonghua <seecode@qq.com>
2021-12-17 18:00:14 +0800

    import-asset

---
+++ 前端工程化/05_模块打包工具/01_webpack/04_import-asset/webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path')
+module.exports = {
+  mode: 'none',
+  entry: './src/main.js',
+  output: {
+    filename: 'index.js',
+    path: path.join(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        // 模块的匹配规则
+        test: /.css$/,
+        // 从后往前执行，每一项都是一个loader
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+}

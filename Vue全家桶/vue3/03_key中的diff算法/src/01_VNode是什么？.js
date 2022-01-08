7b09ed36d35aae5975552ac390a3098bf39b000f
Zhou-Zhonghua <pacpc@qq.com>
2022-01-08 17:15:01 +0800

    VNode是什么

---
+++ Vue全家桶/vue3/03_key中的diff算法/src/01_VNode是什么？.js
@@ -0,0 +1,12 @@
+// <div class="name" style="font-size: 3rem; color: #333;">一碗周</div>
+const vNode = {
+  type: 'div',
+  props: {
+    class: 'name',
+    style: {
+      fontSize: '3rem',
+      color: '#333',
+    },
+  },
+  children: '一碗周',
+}

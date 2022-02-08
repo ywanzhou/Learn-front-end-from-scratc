8903f6c56084defb3a3492c523b7b36711468146
Zhou-Zhonghua <pacpc@qq.com>
2022-02-08 21:43:14 +0800

    emit传值

---
+++ Vue全家桶/vue3/04_component-communication/src/components/emit/child.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="child-wrap input-group">
+    <input
+      v-model="value"
+      type="text"
+      class="form-control"
+      placeholder="请输入"
+    />
+    <div class="input-group-append">
+      <button @click="handleSubmit" class="btn btn-primary" type="button">
+        添加
+      </button>
+    </div>
+  </div>
+</template>
+<script setup>
+import { ref, defineEmits } from 'vue'
+const value = ref('')
+const emits = defineEmits(['add'])
+const handleSubmit = () => {
+  emits('add', value.value)
+  value.value = ''
+}
+</script>
+<style>
+.child-wrap {
+  width: 300px;
+  margin: 0 auto;
+}
+</style>
---
+++ Vue全家桶/vue3/04_component-communication/src/components/emit/parent.vue
@@ -0,0 +1,23 @@
+<template>
+  <!-- 父组件 -->
+  <ul class="parent list-group">
+    <li class="list-group-item" v-for="i in list" :key="i">{{ i }}</li>
+  </ul>
+  <!-- 子组件 -->
+  <child-components @add="handleAdd"></child-components>
+</template>
+<script setup>
+import { ref } from 'vue'
+import ChildComponents from './child.vue'
+const list = ref(['JavaScript', 'HTML', 'CSS'])
+// add 触发后的事件处理函数
+const handleAdd = value => {
+  list.value.push(value)
+}
+</script>
+<style>
+.parent {
+  width: 300px;
+  margin: 100px auto 20px;
+}
+</style>

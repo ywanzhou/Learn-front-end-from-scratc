5dbdc9c5b0d59c655b7e0af7f440d62fb2bfe533
Zhou-Zhonghua <pacpc@qq.com>
2022-02-10 23:34:34 +0800

    删除无意义代码

--- Vue全家桶/vue3/06_composition-api/src/App.vue
+++ Vue全家桶/vue3/06_composition-api/src/App.vue
@@ -1,26 +1,8 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>组件</h1>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+<script setup>
+import { onMounted, ref } from 'vue'
+onMounted(() => {})
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style scoped></style>

06b153803d48ebe09071adc5e5bc3509477e48f1
Zhou-Zhonghua <pacpc@qq.com>
2022-02-11 23:20:45 +0800

    toRefs api

--- Vue全家桶/vue3/06_composition-api/src/App.vue
+++ Vue全家桶/vue3/06_composition-api/src/App.vue
@@ -3,24 +3,21 @@
   <div style="margin: 24px 0">
     <span>姓名：</span>
     <!-- 在模板中，Vue 会自动帮助我们为 ref 进行解包，所以不需要使用 ref.value 的形式 -->
-    <span>{{ readonlyName }}</span>
+    <span>{{ user.name }}</span>
     <br />
     <span>年龄：</span>
-    <span>{{ age }}</span>
+    <span>{{ user.age }}</span>
   </div>
   <button @click="handleEditName">修改姓名</button>
   <br />
   <button @click="handleEditAge">修改年龄</button>
 </template>
 <script setup>
-// 导入 ref
-import { ref, readonly } from 'vue'
-// 创建响应式对象
-const name = ref('一碗粥')
-const age = ref('18')
-const readonlyName = readonly(name)
+import { toRefs, reactive } from 'vue'
+const user = reactive({ name: '一碗粥', age: '18' })
+// user 下的属性通过 toRefs 与解构后的数据建立了链接，任何一个修改都会引起另一个的变化
+const { name, age } = toRefs(user)
 const handleEditName = () => {
-  // 通过 ref 创造的响应式对象，我们需要通过 ref.value 的方式访问
   name.value = '一碗周'
 }
 const handleEditAge = () => {

791332feeee28d086c96b9204464aa44d49fd0eb
Zhou-Zhonghua <pacpc@qq.com>
2022-02-11 23:10:02 +0800

    readonly API

--- Vue全家桶/vue3/06_composition-api/src/App.vue
+++ Vue全家桶/vue3/06_composition-api/src/App.vue
@@ -3,7 +3,7 @@
   <div style="margin: 24px 0">
     <span>姓名：</span>
     <!-- 在模板中，Vue 会自动帮助我们为 ref 进行解包，所以不需要使用 ref.value 的形式 -->
-    <span>{{ name }}</span>
+    <span>{{ readonlyName }}</span>
     <br />
     <span>年龄：</span>
     <span>{{ age }}</span>
@@ -14,10 +14,11 @@
 </template>
 <script setup>
 // 导入 ref
-import { ref } from 'vue'
+import { ref, readonly } from 'vue'
 // 创建响应式对象
 const name = ref('一碗粥')
 const age = ref('18')
+const readonlyName = readonly(name)
 const handleEditName = () => {
   // 通过 ref 创造的响应式对象，我们需要通过 ref.value 的方式访问
   name.value = '一碗周'

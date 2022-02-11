a72570c7164c777f2f8b73a51fb6dc8d4d03d765
Zhou-Zhonghua <pacpc@qq.com>
2022-02-11 23:15:06 +0800

    readonly api

---
+++ Vue全家桶/vue3/06_composition-api/src/03_readonly API.vue
@@ -0,0 +1,29 @@
+<template>
+  <h3>信息展示组件</h3>
+  <div style="margin: 24px 0">
+    <span>姓名：</span>
+    <!-- 在模板中，Vue 会自动帮助我们为 ref 进行解包，所以不需要使用 ref.value 的形式 -->
+    <span>{{ readonlyName }}</span>
+    <br />
+    <span>年龄：</span>
+    <span>{{ age }}</span>
+  </div>
+  <button @click="handleEditName">修改姓名</button>
+  <br />
+  <button @click="handleEditAge">修改年龄</button>
+</template>
+<script setup>
+// 导入 ref
+import { ref, readonly } from 'vue'
+// 创建响应式对象
+const name = ref('一碗粥')
+const age = ref('18')
+const readonlyName = readonly(name)
+const handleEditName = () => {
+  // 通过 ref 创造的响应式对象，我们需要通过 ref.value 的方式访问
+  name.value = '一碗周'
+}
+const handleEditAge = () => {
+  age.value = '20'
+}
+</script>

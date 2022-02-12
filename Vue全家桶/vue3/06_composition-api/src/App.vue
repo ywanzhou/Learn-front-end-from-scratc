d93def3a5f6b8b43b63d4de618c121771cd86a0c
Zhou-Zhonghua <pacpc@qq.com>
2022-02-12 23:54:41 +0800

    生命周期

--- Vue全家桶/vue3/06_composition-api/src/App.vue
+++ Vue全家桶/vue3/06_composition-api/src/App.vue
@@ -1,9 +1,10 @@
 <template>
-  <computed-demo v-model:name="name" />
-  <h3>{{ name }}</h3>
+  <h3 ref="nameRef">一碗周</h3>
 </template>
 <script setup>
-import { ref } from 'vue'
-import ComputedDemo from './components/test.vue'
-const name = ref('一碗周')
+import { ref, onMounted } from 'vue'
+const nameRef = ref(null)
+onMounted(() => {
+  console.log(nameRef.value) // <h3>一碗周</h3>
+})
 </script>

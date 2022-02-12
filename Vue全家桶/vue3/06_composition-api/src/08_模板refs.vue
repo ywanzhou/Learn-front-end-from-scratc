9c9a9a33b9774b0e1894667fd9e144e5a5bff6d9
Zhou-Zhonghua <pacpc@qq.com>
2022-02-12 23:49:06 +0800

    模板refs

---
+++ Vue全家桶/vue3/06_composition-api/src/08_模板refs.vue
@@ -0,0 +1,10 @@
+<template>
+  <h3 ref="nameRef">一碗周</h3>
+</template>
+<script setup>
+import { ref, onMounted } from 'vue'
+const nameRef = ref(null)
+onMounted(() => {
+  console.log(nameRef.value) // <h3>一碗周</h3>
+})
+</script>

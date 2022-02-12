5a75c3dcd8d1c62541047f5fa774b0547343bc1e
Zhou-Zhonghua <pacpc@qq.com>
2022-02-12 23:19:04 +0800

    computed getter

---
+++ Vue全家桶/vue3/06_composition-api/src/06_computed getter.vue
@@ -0,0 +1,13 @@
+<template>
+  <pre>{{ user }}</pre>
+</template>
+<script setup>
+// 引入 computed 方法
+import { ref, computed } from 'vue'
+const name = ref('一碗周')
+const age = ref('18')
+// 定义计算属性
+const user = computed(() => {
+  return `姓名：${name.value}\n年龄：${age.value}`
+})
+</script>

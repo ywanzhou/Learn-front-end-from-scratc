6791849384001830fd30bf8f032ab87d98cac645
Zhou-Zhonghua <pacpc@qq.com>
2022-02-12 23:38:30 +0800

    computed get set 用法

--- Vue全家桶/vue3/06_composition-api/src/App.vue
+++ Vue全家桶/vue3/06_composition-api/src/App.vue
@@ -1,13 +1,9 @@
 <template>
-  <pre>{{ user }}</pre>
+  <computed-demo v-model:name="name" />
+  <h3>{{ name }}</h3>
 </template>
 <script setup>
-// 引入 computed 方法
-import { ref, computed } from 'vue'
+import { ref } from 'vue'
+import ComputedDemo from './components/test.vue'
 const name = ref('一碗周')
-const age = ref('18')
-// 定义计算属性
-const user = computed(() => {
-  return `姓名：${name.value}\n年龄：${age.value}`
-})
 </script>
---
+++ Vue全家桶/vue3/06_composition-api/src/components/test.vue
@@ -0,0 +1,22 @@
+<template>
+  <input type="text" v-model="myName" />
+</template>
+<script setup>
+// 引入需要的方法
+import { defineProps, defineEmits, computed } from 'vue'
+// 定义 props
+const props = defineProps({
+  name: String,
+})
+// v-model 规定写法 update:name name->需要v-model的名称
+const emit = defineEmits(['update:name'])
+// 定义计算属性
+const myName = computed({
+  get() {
+    return props.name
+  },
+  set(val) {
+    emit('update:name', val)
+  },
+})
+</script>

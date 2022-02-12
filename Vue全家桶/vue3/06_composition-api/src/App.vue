e6020ec7bf43ecfeb62e712b53f23690996af3eb
Zhou-Zhonghua <pacpc@qq.com>
2022-02-12 23:18:34 +0800

    computed getter用法

--- Vue全家桶/vue3/06_composition-api/src/App.vue
+++ Vue全家桶/vue3/06_composition-api/src/App.vue
@@ -1,27 +1,13 @@
 <template>
-  <h3>信息展示组件</h3>
-  <div style="margin: 24px 0">
-    <span>姓名：</span>
-    <!-- 在模板中，Vue 会自动帮助我们为 ref 进行解包，所以不需要使用 ref.value 的形式 -->
-    <span>{{ user.name }}</span>
-    <br />
-    <span>年龄：</span>
-    <span>{{ user.age }}</span>
-  </div>
-  <button @click="handleEditName">修改姓名</button>
-  <br />
-  <button @click="handleEditAge">修改年龄</button>
+  <pre>{{ user }}</pre>
 </template>
 <script setup>
-import { toRef, reactive } from 'vue'
-const user = reactive({ name: '一碗粥', age: '18' })
-// user 下的属性通过 toRefs 与解构后的数据建立了链接，任何一个修改都会引起另一个的变化
-const name = toRef(user, 'name')
-const age = toRef(user, 'age')
-const handleEditName = () => {
-  name.value = '一碗周'
-}
-const handleEditAge = () => {
-  age.value = '20'
-}
+// 引入 computed 方法
+import { ref, computed } from 'vue'
+const name = ref('一碗周')
+const age = ref('18')
+// 定义计算属性
+const user = computed(() => {
+  return `姓名：${name.value}\n年龄：${age.value}`
+})
 </script>

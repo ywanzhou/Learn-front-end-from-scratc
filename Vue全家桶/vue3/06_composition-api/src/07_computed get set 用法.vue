d5330e718bf903166cccfc08f0e237ae4f160b74
Zhou-Zhonghua <pacpc@qq.com>
2022-02-12 23:38:45 +0800

    computed get set 用法

---
+++ Vue全家桶/vue3/06_composition-api/src/07_computed get set 用法.vue
@@ -0,0 +1,9 @@
+<template>
+  <computed-demo v-model:name="name" />
+  <h3>{{ name }}</h3>
+</template>
+<script setup>
+import { ref } from 'vue'
+import ComputedDemo from './components/test.vue'
+const name = ref('一碗周')
+</script>

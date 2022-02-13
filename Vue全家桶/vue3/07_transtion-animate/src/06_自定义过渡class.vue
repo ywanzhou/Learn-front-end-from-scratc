99598d1e4e4989dc751c6532c1053b78932e52c9
Zhou-Zhonghua <pacpc@qq.com>
2022-02-13 23:32:43 +0800

    自定义过渡class

---
+++ Vue全家桶/vue3/07_transtion-animate/src/06_自定义过渡class.vue
@@ -0,0 +1,27 @@
+<template>
+  <button class="btn btn-primary" @click="show = !show">显示与隐藏</button>
+  <br />
+  <transition
+    mode="out-in"
+    enter-active-class="animate__animated animate__fadeInDown"
+    leave-active-class="animate__animated animate__fadeOutDown"
+  >
+    <img v-if="show" alt="Vue logo" src="./assets/logo.png" />
+  </transition>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const show = ref(true)
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>

db749da7ec18c2d68c40be967a244f2261fab58f
Zhou-Zhonghua <pacpc@qq.com>
2022-02-13 21:48:49 +0800

    简易的过渡效果

---
+++ Vue全家桶/vue3/07_transtion-animate/src/01_简易的过渡效果.vue
@@ -0,0 +1,43 @@
+<template>
+  <button class="btn btn-primary" @click="helloWorldShow = !helloWorldShow">
+    显示与隐藏
+  </button>
+  <br />
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <transition>
+    <hello-world v-if="helloWorldShow" msg="【一碗周】过渡动画演示demo" />
+  </transition>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+const helloWorldShow = ref(true)
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+/* 进入之前和离开后的样式 */
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+/* 离开和进入过程中的样式 */
+.v-enter-active,
+.v-leave-active {
+  /* 添加过渡动画 */
+  transition: opacity 0.5s ease;
+}
+/* 进入之后和离开之前的样式 */
+.v-enter-to,
+.v-leave-from {
+  opacity: 1;
+}
+</style>

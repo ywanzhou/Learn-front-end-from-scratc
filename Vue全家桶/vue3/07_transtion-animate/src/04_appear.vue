561a6e67c4b6d453016eaeaa57d6643ce5983f58
Zhou-Zhonghua <pacpc@qq.com>
2022-02-13 22:55:22 +0800

    appear

---
+++ Vue全家桶/vue3/07_transtion-animate/src/04_appear.vue
@@ -0,0 +1,38 @@
+<template>
+  <button class="btn btn-primary" @click="show = !show">显示与隐藏</button>
+  <br />
+  <transition mode="out-in" appear>
+    <img v-if="show" alt="Vue logo" src="./assets/logo.png" />
+  </transition>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const show = ref(true)
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+/* 离开和进入过程中的样式 */
+.v-enter-active,
+.v-leave-active {
+  /* 添加过渡动画 */
+  transition: opacity 1.5s ease;
+}
+/* 进入之后和离开之前的样式 */
+.v-enter-to,
+.v-leave-from {
+  opacity: 1;
+}
+</style>

c8fdbf65167cd25db0dd2b5c4ae4152b1ecd1c6c
Zhou-Zhonghua <pacpc@qq.com>
2022-02-14 07:14:57 +0800

    过渡动画

--- Vue全家桶/vue3/07_transtion-animate/src/App.vue
+++ Vue全家桶/vue3/07_transtion-animate/src/App.vue
@@ -1,18 +1,14 @@
 <template>
-  <button class="btn btn-primary" @click="helloWorldShow = !helloWorldShow">
-    显示与隐藏
-  </button>
+  <button class="btn btn-primary" @click="show = !show">显示与隐藏</button>
   <br />
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <transition>
-    <hello-world v-if="helloWorldShow" msg="【一碗周】过渡动画演示demo" />
+  <transition mode="out-in" appear>
+    <img v-if="show" alt="Vue logo" src="./assets/logo.png" />
   </transition>
 </template>
 
 <script setup>
 import { ref } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
-const helloWorldShow = ref(true)
+const show = ref(true)
 </script>
 
 <style>
@@ -24,20 +20,10 @@ const helloWorldShow = ref(true)
   color: #2c3e50;
   margin-top: 60px;
 }
-/* 进入之前和离开后的样式 */
-.v-enter-from,
-.v-leave-to {
-  opacity: 0;
+.v-enter-active {
+  animation: fadeInDown 0.5s;
 }
-/* 离开和进入过程中的样式 */
-.v-enter-active,
 .v-leave-active {
-  /* 添加过渡动画 */
-  transition: opacity 0.5s ease;
-}
-/* 进入之后和离开之前的样式 */
-.v-enter-to,
-.v-leave-from {
-  opacity: 1;
+  animation: fadeOutDown 0.5s;
 }
 </style>

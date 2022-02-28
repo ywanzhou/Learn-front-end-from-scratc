317dd4734fe83b187dd9238fe25b11fd9358a0cf
Zhou-Zhonghua <pacpc@qq.com>
2022-02-28 11:53:24 +0800

    动态路由

---
+++ Vue全家桶/vue-router/LearnVueRouter4/src/components/user.vue
@@ -0,0 +1,8 @@
+<template>
+  <h1>404</h1>
+  <p>{{ params.pathMatch }}</p>
+</template>
+<script setup>
+import { useRoute } from 'vue-router'
+const { params } = useRoute()
+</script>
--- Vue全家桶/vue-router/LearnVueRouter4/src/router.js
+++ Vue全家桶/vue-router/LearnVueRouter4/src/router.js
@@ -1,13 +1,23 @@
 import RootComponent from './components/root.vue'
-export default [
-  {
+export default [{
     path: '/',
     // 引入组件
-    component: RootComponent
+    component: RootComponent,
   },
   {
     path: '/hello',
     // 路由懒加载引入组件
-    component: () => import('./components/HelloWorld.vue')
-  }
+    component: () => import('./components/HelloWorld.vue'),
+    children: [{
+      path: '/about',
+      component: () => import()
+    },{
+      path: '/user',
+      component: () => import()
+    },]
+  },
+  {
+    path: '/:pathMatch(.*)',
+    component: () => import('./components/Page404.vue'),
+  },
 ]

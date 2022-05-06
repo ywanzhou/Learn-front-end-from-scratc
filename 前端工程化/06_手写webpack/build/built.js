265f6b82c98aad586aaf57fad1149579854015c1
Zhou-Zhonghua <pacpc@qq.com>
2022-05-06 22:38:11 +0800

    编写注释

--- 前端工程化/06_手写webpack/build/built.js
+++ 前端工程化/06_手写webpack/build/built.js
@@ -1,22 +1,25 @@
 ;(function (modules) {
   // webpackBootstrap
   // The module cache
+  // 缓存被加载过的模块
   var installedModules = {}
 
   // The require function
+  // 这个方法的作用是根据 modules 中的 key 返回模块的 exports
   function __webpack_require__(moduleId) {
-    // Check if module is in cache
+    // Check if module is in cache 判断缓存中是否存在 存在则直接使用缓存中的
     if (installedModules[moduleId]) {
       return installedModules[moduleId].exports
     }
     // Create a new module (and put it into the cache)
     var module = (installedModules[moduleId] = {
       i: moduleId,
-      l: false,
+      l: false, // 是否为加载 false 表示未加载
       exports: {},
     })
 
     // Execute the module function
+    // 找到 modules 中的具体函数进行调用，执行完毕后会将该模块导出的内容挂载到 module.exports 上
     modules[moduleId].call(
       module.exports,
       module,
@@ -25,9 +28,10 @@
     )
 
     // Flag the module as loaded
-    module.l = true
+    module.l = true // 修改为已经被加载
 
     // Return the exports of the module
+    // 返回模块的 exports
     return module.exports
   }
 
@@ -96,12 +100,13 @@
     return Object.prototype.hasOwnProperty.call(object, property)
   }
 
-  // __webpack_public_path__
+  // __webpack_public_path__ 公共访问路径
   __webpack_require__.p = ''
 
   // Load entry module and return exports
   return __webpack_require__((__webpack_require__.s = './src/index.js'))
 })({
+  // 该对象中的键为模块的id
   './src/index.js':
     /*! no static exports found */
     function (module, exports) {
@@ -109,3 +114,10 @@
       module.exports = 'index.js导出的内容'
     },
 })
+/**
+ * 1. 打包后的文件就是一个自执行函数，调用时传递一个对象
+ * 2. 传递的对象中的键是文件名与路径的拼接，值就是某个模块，它是一个函数
+ * 3. 这个模块与 node.js 中的模块加载有些类似，会将被加载的模块内容包裹在一个函数中
+ * 4. 这个函数在某个时间点被调用，同时会接受一定的参数，利用这些参数实现模块加载操作
+ * 5. modules 这个参数最终的值就是所有模块的一个集合
+ */
